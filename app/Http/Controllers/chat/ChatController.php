0e8dcb68612b1ecec1d1956e0eb3035074e30332
abrahamarslan <arslan@abrahamarslan.com>
2020-11-14 09:12:19 +0530

    #15 - Echo updates

--- app/Http/Controllers/chat/ChatController.php
+++ app/Http/Controllers/chat/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\chat;
 
+use App\Events\ChatInitiationRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\DefaultController;
 use Illuminate\Http\Request;
@@ -10,6 +11,6 @@ use Illuminate\Support\Facades\Redis;
 class ChatController extends DefaultController
 {
     public function getKey() {
-        return Redis::get('TID');
+        ChatInitiationRequest::dispatch();
     }
 }
