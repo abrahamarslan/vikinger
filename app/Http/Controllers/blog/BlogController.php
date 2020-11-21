938e289dedc037ebc995ee16e2cb38ca5ac3dd7a
abrahamarslan <arslan@abrahamarslan.com>
2020-11-21 19:31:19 +0530

    #34 - Shares

---
+++ app/Http/Controllers/blog/BlogController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\blog;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/reaction/ReactionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\reaction;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ReactionController extends Controller
+{
+    //
+}
